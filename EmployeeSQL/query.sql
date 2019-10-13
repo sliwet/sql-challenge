4ae2ae9d4b1f54d09e801ed6fcc6aa19d931c16c
Daewon Kwon <sliwet@gmail.com>
2019-10-13 00:27:31 -0400

    Uploading solutions

---
+++ EmployeeSQL/query.sql
@@ -0,0 +1,64 @@
+﻿-- 1. List: employee number, last name, first name, gender, and salary
+-- e.emp_no,e.last_name,e.first_name,e.gender,s.salary
+-- s.emp_no >- e.emp_no
+
+SELECT e.emp_no,e.last_name,e.first_name,e.gender,s.salary
+FROM employees AS e JOIN salaries AS s ON s.emp_no = e.emp_no;
+
+-- 2. List employees who were hired in 1986
+
+SELECT first_name || ' ' || last_name AS "Employee", hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' AND hire_date <= '1986-12-31';
+
+-- 3. List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number
+-- last name, first name, and start and end employment dates.
+-- dm.dept_no >- d.dept_no
+
+SELECT d.dept_no,d.dept_name,dm.emp_no,e.last_name,e.first_name,dm.from_date,dm.to_date
+FROM departments AS d 
+JOIN dept_manager AS dm ON (dm.dept_no = d.dept_no) 
+JOIN employees AS e ON (dm.emp_no = e.emp_no);
+
+-- 4. List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+-- de.emp_no >- e.emp , de.dept_no >- d.dept_no
+
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e 
+JOIN dept_emp AS de ON (de.emp_no = e.emp_no) 
+JOIN departments AS d ON (de.dept_no=d.dept_no);
+
+-- 5. List all employees whose first name is "Hercules" and last names begin with "B."
+
+SELECT first_name || ' ' || last_name AS "Employee"
+FROM employees
+WHERE first_name = 'Hercules' AND last_name LIKE 'B%';
+
+-- 6. List all employees in the Sales department, including their 
+-- employee number, last name, first name, and department name.
+-- de.emp_no >- e.emp_no , de.dept_no >- d.dept_no
+
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e 
+JOIN dept_emp AS de ON (de.emp_no = e.emp_no) 
+JOIN departments AS d ON (de.dept_no=d.dept_no)
+WHERE d.dept_name = 'Sales';
+
+-- 7. List all employees in the Sales and Development departments, including their 
+-- employee number, last name, first name, and department name.
+-- de.emp_no >- e.emp_no , de.dept_no >- d.dept_no
+
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e 
+JOIN dept_emp AS de ON (de.emp_no = e.emp_no) 
+JOIN departments AS d ON (de.dept_no=d.dept_no)
+WHERE d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+-- 8 In descending order, list the frequency count of employee last names
+-- i.e., how many employees share each last name.
+
+SELECT last_name,COUNT(emp_no) AS "Number of employees" FROM employees
+GROUP BY last_name
+ORDER BY "Number of employees" DESC;
---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,54 @@
+﻿CREATE TABLE departments (
+    dept_no varchar(4)  PRIMARY KEY NOT NULL,
+    dept_name varchar UNIQUE NOT NULL
+);
+
+CREATE TABLE employees (
+    emp_no int PRIMARY KEY NOT NULL,
+    birth_date varchar(10)   NOT NULL,
+    first_name varchar   NOT NULL,
+    last_name varchar   NOT NULL,
+    gender varchar(1)   NOT NULL,
+    hire_date varchar(10)   NOT NULL
+);
+
+CREATE TABLE dept_manager (
+    dept_no varchar(4)   NOT NULL,
+    emp_no int   NOT NULL,
+    from_date varchar(10)   NOT NULL,
+    to_date varchar(10)   NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no,from_date)
+);
+
+CREATE TABLE dept_emp (
+    emp_no int   NOT NULL,
+    dept_no varchar(4)   NOT NULL,
+    from_date varchar(10)   NOT NULL,
+    to_date varchar(10)   NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no,dept_no,from_date)
+
+);
+
+CREATE TABLE titles (
+    emp_no int   NOT NULL,
+    title varchar   NOT NULL,
+    from_date varchar(10)   NOT NULL,
+    to_date varchar(10)   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (emp_no,from_date)
+);
+
+CREATE TABLE salaries (
+    emp_no int PRIMARY KEY  NOT NULL,
+    salary int   NOT NULL,
+    from_date varchar(10)   NOT NULL,
+    to_date varchar(10)   NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+
+
+
